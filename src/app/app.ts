2a299f9160cd25ccb4ab384fe0b795016a5c9184
Donatien Vachette <donatien.vachette@hotmail.com>
2026-09-04 15:17:43 +0200

    TP1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Alert } from "./components/alert/alert";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'iut-root',
+  imports: [RouterOutlet, Alert],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('fix-autoreload');
+  protected readonly title = signal('donatien-forst-project');
+  private readonly maxCounter = 10
+  protected readonly counter = signal<number>(this.maxCounter)
+  constructor() {
+    setInterval(() => { this.counter.set((this.counter() + 1) % this.maxCounter) }, 1000)
+  }
 }
---
+++ src/app/components/alert/alert.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iut-alert',
+  imports: [],
+  templateUrl: './alert.html',
+  styleUrl: './alert.css',
+})
+export class Alert {
+
+}
